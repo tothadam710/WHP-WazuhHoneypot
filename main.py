1284465d19bb7f588a6cb2d47f1e3ed97a55f07d
toth.adam710@gmail.com <adam.toth@greenventions.hu>
2025-09-15 23:23:28 +0200

    resources_folder_lowercase

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import json
+from wazuh.Authentication.Token_generate import generate_JWT_token
+from wazuh.SysCollector.CollectSystemInventory import collect_all_syscollector
+from preprocess.Clean_export import clean_wazuh_export
+#from preprocess.Aggregate import aggregate_honeypot_patterns, aggregate_os_specific
+from preprocess.Separate_OS import split_by_os
+from preprocess.Aggregate import aggregate_os_specific
+
+if __name__=="__main__":
+
+
+    token = generate_JWT_token()
+    data = collect_all_syscollector(token)
+
+    with open("resources/wazuh_export.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+    clean_wazuh_export("resources/wazuh_export.json", "resources/cleaned.json")
+    split_by_os("resources/cleaned.json", "resources/windows_agents.json", "resources/linux_agents.json")
+    aggregate_os_specific("resources/windows_agents.json", "resources/windows_profile.json")
+    aggregate_os_specific("resources/linux_agents.json", "resources/linux_profile.json")
+
+    
+
+
+    
